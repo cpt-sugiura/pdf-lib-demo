8fe96d6c43353ebab947fc32cac5a6c2989bd085
sugiura <sugiura@cpoint-lab.co.jp>
2022-10-25 11:59:51 +0900

    add demo

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,85 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import {makePdfDoc} from "./pdf/makePdfDoc";
+import {PageSizes, PDFFont, PDFPage} from "pdf-lib";
+// @ts-ignore
+import download from "downloadjs";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <button id="pdf-dl" type="button">PDFダウンロード</button>
   </div>
 `
+const btnEl = document.querySelector<HTMLButtonElement>('#pdf-dl');
+if (btnEl) {
+  btnEl.addEventListener('click', async () => {
+    const {pdfDoc, fontIpamp} = await makePdfDoc()
+    const page = pdfDoc.addPage(PageSizes.A4)
+    // XYの原点は左下。高さ = 上辺のY座標
+    const topY = page.getSize().height;
+    const lineHeight = 18;
+    const fontSize = 16;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    const baseY = 35;
+
+    const leftX = 25;
+    ['左揃え', '左ぞろえ二行目', '左揃え3行目'].forEach((text ,i) => {
+      drawTextAlignLeft(text, {leftX, y: topY - (baseY + lineHeight * i), font:fontIpamp, fontSize, page});
+    })
+
+    const rightX = page.getSize().width - 25;
+    ['右揃え', '右ぞろえ二行目', '右揃え3行目'].forEach((text, i) => {
+      drawTextAlignRight(text, {rightX, y: topY - (baseY + lineHeight * i), font: fontIpamp, fontSize, page})
+    });
+
+    const textBoxEdgeX : [number, number] = [0, PageSizes.A4[0]];
+    ['中央揃え', '中央ぞろえ二行目', '中央揃え3行目'].forEach((text, i) => {
+      drawTextAlignCenter(text, {textBoxEdgeX, y: topY - (baseY + lineHeight * i), font: fontIpamp, fontSize, page})
+    })
+
+    const pdfRaw = await pdfDoc.save();
+    download(pdfRaw, "demo.pdf", "application/pdf");
+  })
+}
+
+
+function drawTextAlignCenter(text: string, option: {
+  textBoxEdgeX: [number, number], y: number, fontSize: number, font: PDFFont, page: PDFPage
+}) {
+  const {textBoxEdgeX, y, fontSize, font, page} = option;
+  const boxWidth = textBoxEdgeX[1] - textBoxEdgeX[0];
+  const textWidth = font.widthOfTextAtSize(text, fontSize)
+  page.drawText(text, {
+    size: fontSize,
+    x: textBoxEdgeX[0] + (boxWidth - textWidth) / 2,
+    y: y,
+    font: font
+  })
+}
+
+function drawTextAlignLeft(text: string, option: {
+  leftX: number, y: number, fontSize: number, font: PDFFont, page: PDFPage
+}) {
+  const {
+    leftX, y, fontSize, font, page
+  } = option;
+  page.drawText(text, {
+    x: leftX,
+    y: y,
+    font: font,
+    size: fontSize
+  });
+}
+function drawTextAlignRight(text: string, option: {
+  rightX: number, y: number, fontSize: number, font: PDFFont, page: PDFPage
+}) {
+  const {
+    rightX, y, fontSize, font, page
+  } = option;
+  const textWidth = font.widthOfTextAtSize(text, fontSize);
+  page.drawText(text, {
+    x: rightX - textWidth,
+    y: y,
+    font: font,
+    size: fontSize
+  });
+}
---
+++ src/pdf/makePdfDoc.ts
@@ -0,0 +1,25 @@
+import {PDFDocument, PDFFont} from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
+
+/** 新規PDFインスタンス生成 */
+export const makePdfDoc = async (): Promise<{
+  pdfDoc: PDFDocument,
+  fontIpamp: PDFFont
+}> => {
+  const makePdfDocInstance = async () => {
+    const pdfDoc = await PDFDocument.create();
+    pdfDoc.registerFontkit(fontkit);
+    return pdfDoc;
+  }
+  const getFont = async () => {
+    return await fetch('/fonts/ipamp.ttf').then((res) => res.arrayBuffer());
+  }
+  return await Promise.all([makePdfDocInstance(), getFont()])
+    .then(async ([pdfDoc, fontRaw]) => {
+      // フォント埋め込み
+      const fontIpamp = await pdfDoc.embedFont(fontRaw);
+      return {
+        pdfDoc, fontIpamp
+      }
+    })
+}
